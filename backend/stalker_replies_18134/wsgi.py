c037262aafb95f8bb36e29210bee2eda5234d608
Crowdbotics <team@crowdbotics.com>
2020-06-16 02:28:21 +0000

    Update models

--- backend/stalker_replies_18134/wsgi.py
+++ backend/stalker_replies_18134/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stalker_replies_18134.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stalker_replies_18134.settings")
 
 application = get_wsgi_application()
