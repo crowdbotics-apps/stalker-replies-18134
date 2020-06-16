827a345c4df9273bd7c767bd6c607a6351935c92
Crowdbotics <team@crowdbotics.com>
2020-06-16 02:27:07 +0000

    Initial commit.

---
+++ backend/stalker_replies_18134/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for stalker_replies_18134 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stalker_replies_18134.settings')
+
+application = get_wsgi_application()
